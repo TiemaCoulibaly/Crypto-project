9bef2238fa46b2d073d5b2c952dca71fa85330ea
Tiema Coulibaly <tiemacouly@outlook.com>
2021-06-07 17:02:20 +0200

    create card component for crypto

---
+++ src/components/cryptoCard/CryptoItem.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+
+import "./cryptoItem.scss";
+const CryptoItem = ({ name, price, value }) => {
+	const [chartData, setChartData] = useState({});
+	const chart = () => {
+		setChartData({
+			labels: ["Bitcoin", "Ethereum", "Bnb", "Usdt", "Other"],
+			datasets: [
+				{
+					label: "Le cours des cryptomonnaies",
+					data: [19, 12, 5, 3, 2],
+					backgroundColor: [
+						"rgba(75, 192, 192, 0.2)",
+						"rgba(75, 192, 192, 0.2)",
+						"rgba(75, 192, 192, 0.2)",
+						"rgba(75, 192, 192, 0.2)",
+						"rgba(75, 192, 192, 0.2)",
+					],
+					borderColor: [
+						"rgba(75, 192, 192, 1)",
+						"rgba(75, 192, 192, 1)",
+						"rgba(75, 192, 192, 1)",
+						"rgba(75, 192, 192, 1)",
+						"rgba(75, 192, 192, 1)",
+					],
+					borderWidth: 1,
+				},
+			],
+		});
+	};
+
+	useEffect(() => {
+		chart();
+	}, []);
+	return (
+		<figure>
+			<h2>{name}</h2>
+			<h3>{price}€</h3>
+			<div className="chart" style={{ width: "100%", height: "22vh" }}>
+				<Line
+					data={chartData}
+					options={{
+						responsive: true,
+					}}
+				/>
+			</div>
+			<figcaption>{value}%</figcaption>
+		</figure>
+	);
+};
+export default CryptoItem;
---
+++ src/components/helpers/CryptoList.js
@@ -0,0 +1,32 @@
+export const CryptoList = [
+	{
+		name: "Bitcoin",
+		price: "40 000",
+		value: "5.4",
+	},
+	{
+		name: "Ethereum",
+		price: "20 000",
+		value: "3.1",
+	},
+	{
+		name: "Dogecoin",
+		price: "240 000",
+		value: "1.2",
+	},
+	{
+		name: "Litecoin",
+		price: "350 000",
+		value: "0.2",
+	},
+	{
+		name: "Binancecoin",
+		price: "8 000",
+		value: "0.9",
+	},
+	{
+		name: "Tether",
+		price: "98 000",
+		value: "5.9",
+	},
+];
