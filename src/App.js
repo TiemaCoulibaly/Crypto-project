295cfea3217107490b6de8f2663efb94d2ea05d8
Mat-Smz <59562144+Mat-Smz@users.noreply.github.com>
2021-06-09 15:25:59 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
-<<<<<<< HEAD
-import Navigation from "./components/router/Navigation";
-import Routes from "./components/router/routes";
-=======
 import Navigation from "./components/navbar/Navigation";
 import Routes from "./components/router/Routes";
->>>>>>> 380a26ffadf50c6e6ee8ae0ed1a906a3c46b2465
 import Footer from "./components/footer/Footer";
 
 function App() {
