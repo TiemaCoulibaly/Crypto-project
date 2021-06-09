e025d8218b9bf1c9cdfc95308a9b0d159cea9f34
Mehdi-Adel <zeadel.mehdi@gmail.com>
2021-06-09 16:06:03 +0200

    Ajout components Auth Register ResetPassword ForgotPassword Private

--- src/components/router/routes.js
+++ src/components/router/routes.js
@@ -5,9 +5,18 @@ import Login from "../login/Login";
 import Portfolio from "../portfolio/Portfolio";
 import Currency from "../currency/Currency";
 import NotFound from "../notfound/NotFound";
+import Auth from "../auth/Auth";
+import Register from "../register/Register";
+import ForgotPassword from "../forgotPassword/ForgotPassword";
+import ResetPassword from "../resetPassword/ResetPassword";
+import Private from "../private/Private";
 
+
+// Routing
 import PublicRouter from "./PublicRouter";
 import PrivateRouter from "./PrivateRouter";
+import PrivateRoute from "../routing/PrivateRoute";
+
 
 const Routes = () => {
 	return (
@@ -17,10 +26,14 @@ const Routes = () => {
 			<PrivateRouter path="/home" component={Home} />
 			<PrivateRouter path="/portfolio" component={Portfolio} />
 			<Route path="/currency" component={Currency} />
+			<PublicRouter exact path="/auth" component={Auth} />
+			<PublicRouter exact path="/register" component={Register} />
+			<PublicRouter exact path="/forgotpassword" component={ForgotPassword} />
+			<PublicRouter exact path="/resetpassword:resetToken" component={ResetPassword} />
+			<PrivateRouter exact path="/private" component={Private} />
 
-			<PublicRouter exact path="*" component={NotFound} />
+			<PublicRouter exact path="*" component={NotFound} />      
 		</Switch>
-		
 	);
 };
 
